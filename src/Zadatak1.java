c4c4ca10a83d7d740685d6f48967c0e8b3ed74c1
IgorPribanic <igor.pribanic.1@gmail.com>
2025-11-03 18:39:17 +0100

    Zadatak 1

---
+++ src/Zadatak1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        String korisnickiUnos = "";
+        Scanner sc = new Scanner(System.in);
+        int slova = 0;
+        int brojevi = 0;
+        int ostaliZnakovi = 0;
+
+        System.out.print("Upišite tekst za koji želite ispisati statistiku znakova: ");
+
+        korisnickiUnos = sc.nextLine();
+
+        while(korisnickiUnos.length() == 0) {
+            System.out.print("\nGreška u radu. Upišite tekst za koji želite ispisati statistiku znakova: ");
+            korisnickiUnos = sc.nextLine();
+        }
+
+        char[] znakovi = new char[korisnickiUnos.length()];
+        znakovi = korisnickiUnos.toCharArray();
+
+        for (int i = 0; i < znakovi.length; i++) {
+            if(Character.isLetter(znakovi[i])){
+                slova +=1;
+            } else if(Character.isDigit(znakovi[i])){
+                brojevi +=1;
+            } else{
+                ostaliZnakovi +=1;
+            }
+        }
+
+        System.out.printf("Za uneseni tekst (" + korisnickiUnos + ") statistika korištenih znakova je:");
+        System.out.printf("\n\t Slova: " + slova);
+        System.out.printf("\n\t Brojevi: " + brojevi);
+        System.out.printf("\n\t Ostali znakovi: " + ostaliZnakovi);
+
+    }
+}
